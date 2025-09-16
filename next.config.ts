139619bc6b59d6c3784dada245a85fad1756c481
colburn-anderson <colburnanderson@gmail.com>
2025-09-16 11:33:18 -0400

    prepare for deployment on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  async rewrites() {
+    if (process.env.NODE_ENV !== "production") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8000/api/:path*", // ← proxy to FastAPI in dev
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
